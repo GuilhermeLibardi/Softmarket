6c45fd7f561c99b3cc3480be01cda8d70c60eef2
Italo <italonoe@hotmail.com>
2018-07-04 04:44:13 -0300

    Vendas 1.0

--- src/app/controllers/VendasController.java
+++ src/app/controllers/VendasController.java
@@ -5,13 +5,18 @@ import app.classes.Produto;
 import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
 
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.MouseEvent;
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.ResourceBundle;
 
 public class VendasController {
@@ -48,6 +53,13 @@ public class VendasController {
     @FXML
     private TableColumn<Produto, Integer> colEstoque;
 
+    private Integer verificador=0;
+
+    @FXML
+    private Button inserir;
+
+    private String statusVenda=("Ocioso");
+
 
 
     @FXML
@@ -57,15 +69,23 @@ public class VendasController {
                 txtCodBarras.setText(newValue.replaceAll("[^\\d]", ""));
             }
         });
-
-
         if(event.getCode().equals(KeyCode.ENTER)) {
-            Main.estoqueProdutos.add(new Produto("Chapolim", 1, 1, 1.2, "24"));
             for(Produto procurar : Main.estoqueProdutos) {
                 if (procurar.getCodigo().equals(txtCodBarras.getText())) {
-                    listaProdutos.add(procurar);
-                    txtQuantidade.requestFocus();
-                    return;
+                    if (!verificaLista(listaProdutos)){
+                        if (verificador == 0) {
+                            verificador++;
+                        }
+                        txtQuantidade.requestFocus();
+                        return;
+                    }else{
+                        Alert erro1 = new Alert(Alert.AlertType.ERROR);
+                        erro1.setTitle("Produto já inserido!");
+                        erro1.setHeaderText("Você está tentando inserir um produto já inserido");
+                        erro1.setContentText("Caso queira aumentar a quantidade remova o produto e insira novamente.");
+                        erro1.showAndWait();
+                        return;
+                    }
                 }
             }
             Alert erroCdB = new Alert(Alert.AlertType.ERROR);
@@ -78,16 +98,25 @@ public class VendasController {
 
     @FXML
     void qntVenda(KeyEvent event) {
-        txtCodBarras.textProperty().addListener((ObservableValue<? extends String> observable, String oldValue, String newValue) -> {
+        txtQuantidade.textProperty().addListener((ObservableValue<? extends String> observable, String oldValue, String newValue) -> {
             if (!newValue.matches("\\d*")) {
-                txtCodBarras.setText(newValue.replaceAll("[^\\d]", ""));
+                txtQuantidade.setText(newValue.replaceAll("[^\\d]", ""));
             }
         });
         if(event.getCode().equals(KeyCode.ENTER)) {
-            if(listaProdutos.get(listaProdutos.size()-1).getQuantidade() >= Integer.parseInt(txtQuantidade.getText())){
-                creatTable();
-                return;
+            for(Produto procurar : Main.estoqueProdutos) {
+                if (procurar.getCodigo().equals(txtCodBarras.getText())) {
+                    if(procurar.getQuantidade()>=Integer.parseInt(txtQuantidade.getText())){
+                        inserir.requestFocus();
+                        if (verificador==1){
+                            verificador++;
+                        }
+                        return;
+                    }
+
+                }
             }
+
             Alert erroEst = new Alert(Alert.AlertType.ERROR);
             erroEst.setTitle("Estoque Indisponível!");
             erroEst.setHeaderText("Quantidade desejada indisponível no estoque.");
@@ -96,8 +125,25 @@ public class VendasController {
         }
     }
 
+    @FXML
+    public void inserirProduto(){
+        if (verificador == 2) {
+            statusVenda = ("Ativa");
+            verificador=0;
+            for(Produto procurar : Main.estoqueProdutos){
+                if (procurar.getCodigo().equals(txtCodBarras.getText())){
+                    procurar.setQuantidade(Integer.parseInt(txtQuantidade.getText()));
+                    listaProdutos.add(procurar);
+                }
+            }
+            txtQuantidade.clear();
+            txtCodBarras.clear();
+            creatTable();
+        }
+    }
 
-    private void creatTable() {
+
+    public void creatTable() {
         colCodBarras.setCellValueFactory(new PropertyValueFactory<Produto, String>("codigo"));
         colNome.setCellValueFactory(new PropertyValueFactory<Produto, String>("nome"));
         colEstoque.setCellValueFactory(new PropertyValueFactory<Produto, Integer>("quantidade"));
@@ -107,6 +153,14 @@ public class VendasController {
     }
 
 
+    public boolean verificaLista(ObservableList<Produto> listaP){
+        for (Produto produtos : listaP){
+            if(produtos.getCodigo().equals(txtCodBarras.getText())){
+                return true;
+            }
+        }
+        return false;
+    }
 }
 
 
