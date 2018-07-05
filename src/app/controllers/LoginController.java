14809b6f4000bff191a9c9e89adebfa988a17c64
Guilherme Libardi <guilherme.libardig@gmail.com>
2018-07-05 16:00:42 -0300

    Login utilizando as classes de usuário para permitir identificação no controller de vendas.

--- src/app/controllers/LoginController.java
+++ src/app/controllers/LoginController.java
@@ -9,6 +9,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.geometry.Rectangle2D;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.stage.Screen;
@@ -24,17 +25,30 @@ public class LoginController {
     @FXML
     TextField txtSenha;
 
-    public void handleLogin() throws Exception {
+    @FXML
+    public void handleLogin() throws java.io.IOException {
         for (Usuario user : Main.usuariosCadastrados) {
             if (txtUsuario.getText().equals(user.getLogin()) && txtSenha.getText().equals(user.getSenha())) {
                 if (user instanceof Gerente) changeScreen("../resources/fxml/telaGerente.fxml");
                 else if (user instanceof Vendedor) changeScreen("../resources/fxml/telaVendas.fxml");
+                return;
             }
         }
+        Alert erroEst = new Alert(Alert.AlertType.ERROR);
+        erroEst.setTitle("Erro de login");
+        erroEst.setHeaderText("Erro ao tentar logar");
+        erroEst.setContentText("Usuário e/ou senha incorretos");
+        erroEst.showAndWait();
     }
 
+    @FXML
     public void handleSubmitSenha() throws Exception {
         handleLogin();
+    }
+
+    @FXML
+    public void handleEnterUsuario() {
+        txtSenha.requestFocus();
     }
 
     private void changeScreen(String fxml) throws IOException {
@@ -50,8 +64,10 @@ public class LoginController {
         Scene scene = new Scene(root);
         stage.setScene(scene);
         stage.show();
-        stage.setResizable(false);
-
+        stage.setResizable(true);
+        stage.setMaximized(true);
+        if (fxml.contains("Gerente")) stage.setTitle("Sistema de gerenciamento");
+        else if (fxml.contains("Vendas")) stage.setTitle("Sistema de vendas");
         stage.setX(bounds.getMinX());
         stage.setY(bounds.getMinY());
         stage.setWidth(bounds.getWidth());
