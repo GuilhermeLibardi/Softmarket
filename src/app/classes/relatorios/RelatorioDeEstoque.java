28b352bece6caf4c44539a38e2482a172c191a58
Guilherme Libardi <guilherme.libardig@gmail.com>
2018-07-12 02:54:54 -0300

    Alert criado para mostrar os relatórios de estoque

--- src/app/classes/relatorios/RelatorioDeEstoque.java
+++ src/app/classes/relatorios/RelatorioDeEstoque.java
@@ -5,8 +5,7 @@ import app.classes.Produto;
 import javafx.collections.ObservableList;
 import javafx.scene.control.Alert;
 
-import java.io.*;
-import java.net.URL;
+import java.io.IOException;
 import java.time.LocalDate;
 
 public class RelatorioDeEstoque extends Relatorio {
@@ -21,11 +20,12 @@ public class RelatorioDeEstoque extends Relatorio {
         //Estrutura do relatório
         int quant = 0;
         float valorc = 0, valorl = 0;
-        System.out.println("Gerando relatório de estoque...");
-
+        StringBuilder sb = new StringBuilder();
+        String s;
         for (Produto p : Main.estoqueProdutos)
         {
-            System.out.println("Produto:" + p.getNome() + " Código:" + p.getCodigo() + " Quantidade:" +  p.getQuantidade() + "\n");
+            s = "Produto: " + p.getNome() + " Código: " + p.getCodigo() + " Quantidade: " +  p.getQuantidade() + "\n";
+            sb.append(s);
             quant += p.getQuantidade();
             valorc += p.getQuantidade() * p.getValorCusto();
             valorl += p.getQuantidade() * p.getValorVenda();
@@ -33,13 +33,11 @@ public class RelatorioDeEstoque extends Relatorio {
 
         Alert alerta = new Alert(Alert.AlertType.INFORMATION);
         alerta.setTitle("Relatório de estoque");
-        alerta.setHeaderText("Vendas de estoque atual (" + LocalDate.now().toString() + ")");
+        alerta.setHeaderText("Relatório de estoque atual (" + LocalDate.now().toString() + ")");
         alerta.setContentText("Quantidade total de produtos: " + quant + "\n" +
                 "Custo total dos itens no estoque: " + valorc + "\n" +
-                "Valor total de venda dos itens: " + valorl + "\nLucro total após vendas: " + (valorl-valorc));
-        for (Produto p : Main.estoqueProdutos) {
-            alerta.setContentText("Produto:" + p.getNome() + " Código:" + p.getCodigo() + " Quantidade:" + p.getQuantidade() + "\n");
-        }
+                "Valor total de venda dos itens: " + valorl + "\nLucro total após vendas: " + (valorl-valorc) + "\n\n" + sb.toString());
+
         alerta.showAndWait();
 
 
